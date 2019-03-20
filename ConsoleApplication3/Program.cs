74c924b27b239f079123899d13144af46a210afb
wuy <529032234@qq.com>
2019-03-20 16:30:12 +0800

    first

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbHelp.Base;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbBase<Person> db = new DbBase<Person>();
+            var model = new Person()
+            {
+                LastName = "last",
+                FirstName = "first",
+                HireDate = DateTime.Now,
+                EnrollmentDate = DateTime.Now,
+                Discriminator = "djf"
+            };
+            db.Insert(model);
+            db.SaveChanges();
+
+           
+
+            var account = db.FindById(1);
+            Console.WriteLine(account.FirstName);
+
+          
+        }
+    }
+}
